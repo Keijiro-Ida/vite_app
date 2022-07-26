7cff33a506fc692d4f49fd7c7801b17d7f7a050b
Keijiro_Ida <idatt1122@gmail.com>
2022-07-26 09:29:15 +0900

    20220726_practice

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,61 @@
 <template>
-  <div class="alert alert-info">
-      <h1>{{title}}</h1>
-      <p ref="msg">{{message}}</p>
-      <button class="btn btn-primary" v-on:click="DoAction">Click</button>
+  <div class="alert alert-primary">
+      <h1>{{data.title}}</h1>
+      <p class="mt-3 h5">{{ $store.state.message}}</p>
+      <hr>
+      <div class="btn btn-secondary"
+        @click="$store.commit('doit')">
+      <a class="h5" @click.stop="$store.commit('reset')">
+        clicked: {{ $store.state.counter }}
+      </a>
+      </div>
   </div>
 
 </template>
 
 <script>
+import { ref, reactive} from 'vue'
+
 export default {
-  name: 'HelloWorld',
-  data() {
+  setup(props) {
+    const data = reactive({
+      title: 'Vuex',
+    })
     return {
-      title: 'HelloWorld',
-      message: 'This is sample message',
-    }
-  },
-  mounted() {
-    this.counter = 0
-  },
-  methods: {
-    DoAction() {
-      this.counter++
-      this.$refs.msg.innerHTML += '<h6>counted: ' + this.counter + '</h6>'
+      data
     }
   }
 }
 </script>
+
+<style>
+.area {
+    width:300px;
+  height:100px;
+  background-color: #fff;
+  padding:10px;
+  font-size:20pt;
+}
+pre {
+  font-size:16pt;
+  line-height: 1.25;
+}
+div.out {
+  padding: 0px;
+  background-color: #eee;
+  width:300px;
+  height:200px;
+}
+div.mid {
+  padding: 0px;
+  background-color: #ddd;
+  width:200px;
+  height:170px;
+}
+div.in {
+  padding: 0px;
+  background-color: #ccc;
+  width:100px;
+  height:140px;
+}
+</style>
